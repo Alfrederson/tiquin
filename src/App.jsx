666657595867654892f9f434f76482c673a0cbec
Alfrederson <aroldorosenberg@gmail.com>
2024-02-01 00:30:30 -0300

    tiquin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { useSqueak } from './hooks/use-squeak'
+import React from 'react'
+
+function App() {
+  const squeak = useSqueak()
+  return (
+    <>
+      <button onClick={squeak}>squeak</button>
+    </>
+  )
+}
+
+export default App
